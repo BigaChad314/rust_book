d6de6c1c07f7cefd5d77245e49dca8deb64e7855
Minjun Kim <minjun@pi-lab.io>
2024-10-18 21:05:44 +0900

    new line

--- Tuts/hello.rs
+++ Tuts/hello.rs
@@ -3,4 +3,9 @@ fn main() {
 
     // Print text to the console.
     println!("Hello World!");
+    println!("I'm a Rustacean!");
 }
+
+// A binary can be generated using the Rust compiler: rustc.
+// Step.1 "rustc hello.rs" -> 로 compile
+// Step.2 compile 된 hello file을 "./hello"로 실행
