aed3916d14397c2f03c87b4e07e98ed882b23256
Minjun Kim <minjun@pi-lab.io>
2024-10-18 21:53:42 +0900

    cargo instructions annotation

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// Building cargo: "cargo build" -> "./target/debug/hello_cargo"
+// Running cargo: "cargo run"
+// Checking the code: "cargo check" -> compile이 되는지 여부만 확인하여 훨씬 빠름.
+
+// 정리:
+// We can create a project using cargo new.
+// We can build a project using cargo build.
+// We can build and run a project in one step using cargo run.
+// We can build a project without producing a binary to check for errors using cargo check.
+// Instead of saving the result of the build in the same directory as our code, Cargo stores it in the target/debug directory.
