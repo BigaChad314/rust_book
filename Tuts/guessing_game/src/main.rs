94636d0fe7b6a62daff405be3f67620cbde08317
Minjun Kim <minjun@pi-lab.io>
2024-10-19 15:47:43 +0900

    guessing game pt.1

--- Tuts/guessing_game/src/main.rs
+++ Tuts/guessing_game/src/main.rs
@@ -11,8 +11,14 @@ fn main() {
     let mut guess = String::new();
 
     io::stdin()
-        .read_line(&mut guess)
+        .read_line(&mut guess) // take an input & append to a string
+                                // "&"mut: argument가 reference라는 뜻. (default: immutable.)
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    let x = 5;
+    let y = 10;
+
+    println!("x ={x} and y + 2 = {}", y+2)
 }
