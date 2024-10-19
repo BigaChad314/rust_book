a9a87bcab2306edb8f840ec12d0b115d9664d53d
Minjun Kim <minjun@pi-lab.io>
2024-10-19 16:55:33 +0900

    step 4. allowing loop for guessing

--- Tuts/guessing_game/src/main.rs
+++ Tuts/guessing_game/src/main.rs
@@ -11,25 +11,29 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    // Part 4. Loop을 통해 Guess를 여러번 반복할 수 있게.
+    loop {
+        println!("Please input your guess.");
 
-    // creating a variable that stores input.
-    // "mut": mutable
-    let mut guess = String::new();
+        // creating a variable that stores input.
+        // "mut": mutable
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess) // take an input & append to a string
-                                // "&"mut: argument가 reference라는 뜻. (default: immutable.)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess) // take an input & append to a string
+                                    // "&"mut: argument가 reference라는 뜻. (default: immutable.)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!"); // "Shadowing": input을 u32 int. 로 바꾼다.
+        // "Shadowing": input을 u32 int. 로 바꾼다.
+        // trim.(): slice랑 유사. 공백을 지운다.
+        // parse.(): string을 다른 type으로 변환한다. 이 경우, guess: u32 로변환 (32-bit integer)
+        let guess: u32 = guess.trim().parse().expect("Please type a number!"); 
 
-    println!("You guessed: {}", guess);
-
-    // Part 3. Matching guess
-    match guess.cmp(&secret_number) { // 'guess' 입력 값과 'secret_number'를 비교. 
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // Part 3. Matching guess
+        match guess.cmp(&secret_number) { // 'guess' 입력 값과 'secret_number'를 비교. 
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
