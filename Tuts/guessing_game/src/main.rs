9986b3b29574570666312853b9522595c961d3ba
Minjun Kim <minjun@pi-lab.io>
2024-10-19 16:33:33 +0900

    Part3. Matching guess w/ parsing guess into u32

--- Tuts/guessing_game/src/main.rs
+++ Tuts/guessing_game/src/main.rs
@@ -1,8 +1,15 @@
 // Bringing input/output (io) from standard (std) library
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering; // for enum. 값 비교: less, greater, equal.
+
 
 fn main() {
     println!("Guess the number!");
+
+    // Part 2. Generating random number
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -15,10 +22,14 @@ fn main() {
                                 // "&"mut: argument가 reference라는 뜻. (default: immutable.)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!"); // input을 u32 int. 로 바꾼다.
+
     println!("You guessed: {}", guess);
 
-    let x = 5;
-    let y = 10;
-
-    println!("x ={x} and y + 2 = {}", y+2)
+    // Part 3. Matching guess
+    match guess.cmp(&secret_number) { // 'guess' 입력 값과 'secret_number'를 비교. 
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
