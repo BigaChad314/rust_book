f44bddced85ac3ad3be4f16740bd2d1bc36217b9
Minjun Kim <minjun@pi-lab.io>
2024-10-20 16:21:59 +0900

    statements & expressions

--- Tuts/functions/src/main.rs
+++ Tuts/functions/src/main.rs
@@ -1,3 +1,4 @@
+// Original functions: basis
 // fn main() {
 //     println!("Hello, world!");
 
@@ -9,10 +10,31 @@
 // }
 
 // Adding Parameters
-fn main() {
-    another_function(5);
-}
+// fn main() {
+//     another_function(5);
+// }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+// fn another_function(x: i32) {
+//     println!("The value of x is: {x}");
+// }
+
+// // Multiple Parameters
+// fn main() {
+//     print_labeled_measurement(5, 'h');
+// }
+
+// fn print_labeled_measurement(value: i32, unit_label: char) {
+//     println!("The measurement is: {value}{unit_label}");
+// }
+
+// Statements & Expressions
+fn main() {
+    // y: statement
+    let y = {
+        // x: expression. Returns x+1. -> the value then bounds to y.
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
 }
