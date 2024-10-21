aae8bb81c1d94dd2bfc9bf139413cbed61824772
Minjun Kim <minjun@pi-lab.io>
2024-10-21 17:28:11 +0900

    4.1 ownership done

---
+++ Rust_book_projects/ownership/ownership2/target/debug/deps/ownership2-42ba52920fb57650.d
@@ -0,0 +1,5 @@
+/Users/minjun/Desktop/rust/Rust_book_projects/ownership/ownership2/target/debug/deps/ownership2-42ba52920fb57650: src/main.rs
+
+/Users/minjun/Desktop/rust/Rust_book_projects/ownership/ownership2/target/debug/deps/ownership2-42ba52920fb57650.d: src/main.rs
+
+src/main.rs:
