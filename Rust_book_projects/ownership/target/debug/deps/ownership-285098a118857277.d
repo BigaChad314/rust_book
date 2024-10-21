fe24e9efb9ecbf83cc6a70e0582147668848140e
Minjun Kim <minjun@pi-lab.io>
2024-10-21 16:48:11 +0900

    string type ownership

---
+++ Rust_book_projects/ownership/target/debug/deps/ownership-285098a118857277.d
@@ -0,0 +1,5 @@
+/Users/minjun/Desktop/rust/Rust_book_projects/ownership/target/debug/deps/ownership-285098a118857277: src/main.rs
+
+/Users/minjun/Desktop/rust/Rust_book_projects/ownership/target/debug/deps/ownership-285098a118857277.d: src/main.rs
+
+src/main.rs:
