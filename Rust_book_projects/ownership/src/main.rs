f6b7f326abb7e6d844b78bb476311b5f3fb55807
Minjun Kim <minjun@pi-lab.io>
2024-10-21 17:06:43 +0900

    heap move

--- Rust_book_projects/ownership/src/main.rs
+++ Rust_book_projects/ownership/src/main.rs
@@ -2,6 +2,13 @@ fn main() {
     let mut s = String::from("hello");
 
     s.push_str(", world!"); // push_str() appends a literal to a String
-
     println!("{s}"); // This will print `hello, world!`
+
+    string_heap_move();
+}
+
+fn string_heap_move() {
+    let s1 = String::from("this is how moving heap works");
+    let s2 = s1;
+    println!("{s2}");
 }
