f2e82ec8b406ab3c552968a5042b99bb7d1e673b
Minjun Kim <minjun@pi-lab.io>
2024-10-21 17:16:24 +0900

    String: ownership. Integer: copy.

--- Rust_book_projects/ownership/src/main.rs
+++ Rust_book_projects/ownership/src/main.rs
@@ -4,11 +4,37 @@ fn main() {
     s.push_str(", world!"); // push_str() appends a literal to a String
     println!("{s}"); // This will print `hello, world!`
 
-    string_heap_move();
+    // string_heap_move();
+    ownership_in_function();
 }
 
 fn string_heap_move() {
     let s1 = String::from("this is how moving heap works");
-    let s2 = s1;
-    println!("{s2}");
+    let s2 = s1.clone();
+    println!("s1 = {s1}, and... s2 = {s2} as well");
 }
+
+fn ownership_in_function() {
+    let s = String::from("taking ownership");  // s comes into scope
+
+    takes_ownership(s);             // s's value moves into the function...
+                                    // ... and so is no longer valid here
+
+    let x = 5;                      // x comes into scope
+
+    makes_copy(x);
+    println!("testing the copy: {x}")  // x would move into the function,
+                                    // but i32 is Copy, so it's okay to still
+                                    // use x afterward
+
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{some_string}");
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{some_integer}");
+} // Here, some_integer goes out of scope. Nothing special happens.
