09b04125cf9eebbb478a49c4395c05dd870a86a5
Minjun Kim <minjun@pi-lab.io>
2024-10-21 10:07:49 +0900

    rebasing git

---
+++ Rust_book_projects/variables/target/debug/deps/variables-40482b15803e59b9.d
@@ -0,0 +1,5 @@
+/Users/minjun/Desktop/rust/Tuts/variables/target/debug/deps/libvariables-40482b15803e59b9.rmeta: src/main.rs
+
+/Users/minjun/Desktop/rust/Tuts/variables/target/debug/deps/variables-40482b15803e59b9.d: src/main.rs
+
+src/main.rs:
