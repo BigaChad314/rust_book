81a218e0a6e9e5ef0520379425bf3ab9428fb123
Minjun Kim <minjun@pi-lab.io>
2024-10-23 16:16:16 +0900

    4.3 slice type init

---
+++ Rust_book_projects/slice_type/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    first_word("lfgb");
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
