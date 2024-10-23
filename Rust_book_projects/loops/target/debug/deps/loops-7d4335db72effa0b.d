0f4662c6dbefdcb71520f99f2c2309f21d6d1a51
Minjun Kim <minjun@pi-lab.io>
2024-10-23 17:18:26 +0900

    returning length with byte-literal

--- Rust_book_projects/loops/target/debug/deps/loops-7d4335db72effa0b.d
+++
@@ -1,5 +0,0 @@
-/Users/minjun/Desktop/rust/Rust_book_projects/loops/target/debug/deps/loops-7d4335db72effa0b: src/main.rs
-
-/Users/minjun/Desktop/rust/Rust_book_projects/loops/target/debug/deps/loops-7d4335db72effa0b.d: src/main.rs
-
-src/main.rs:
--- Rust_book_projects/ownership/target/debug/ownership.d
+++
@@ -1,1 +0,0 @@
-/Users/minjun/Desktop/rust/Rust_book_projects/ownership/target/debug/ownership: /Users/minjun/Desktop/rust/Rust_book_projects/ownership/src/main.rs
